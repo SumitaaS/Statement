f14af750c6237c9360b64f2e192a42ad46efcdd4
SumitaaS <106767072+SumitaaS@users.noreply.github.com>
2022-06-27 18:27:49 +0530

    Updated

--- src/main/java/com/sbi/statement/layer4/StatementServiceImplementation.java
+++ src/main/java/com/sbi/statement/layer4/StatementServiceImplementation.java
@@ -23,32 +23,20 @@ public class StatementServiceImplementation implements StatementService {
 	@Autowired
 	TransactionsRepository tranRepo;
 
+	
 	@Override
-	public List<Transactions> getMonthlyStatement(int acct_no, int month, int year) {
+	public List<Transactions> getCustomStatementEmail(LocalDate date1, LocalDate date2, String email) {
+		
+		
 		Accounts a = new Accounts();
-		a.setAccountNumber(acct_no);
-		LocalDate startDate = LocalDate.of(year, month, 01);
-		LocalDate endDate = startDate.withDayOfMonth(startDate.getMonth().length(startDate.isLeapYear()));
-
-		List<Transactions> tl = tranRepo.findByTransTimeBetweenAndAcct(startDate, endDate, a);
-		System.out.println("\t1.size" + tl.size());
-		return tl;
-	}
-
-	@Override
-	public List<Transactions> getYearlyStatement(int accNo, int year) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Transactions> getCustomStatement(LocalDate date1, LocalDate date2, int acct_no) {
-		Accounts a = new Accounts();
-		a.setAccountNumber(acct_no);
+		a.setEmail(email);
+		a = acctRepo.findByEmail(email);
 
 		List<Transactions> tl = tranRepo.findByTransTimeBetweenAndAcct(date1, date2, a);
 		System.out.println("\t1.size" + tl.size());
 		return tl;
 	}
+
+
 
 }
