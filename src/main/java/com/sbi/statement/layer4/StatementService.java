e2c605d3b2d33519b27a4f599d82a78dee40f568
SumitaaS <106767072+SumitaaS@users.noreply.github.com>
2022-06-27 18:27:08 +0530

    Updated

--- src/main/java/com/sbi/statement/layer4/StatementService.java
+++ src/main/java/com/sbi/statement/layer4/StatementService.java
@@ -14,12 +14,5 @@ import com.sbi.statement.layer2.Transactions;
 @Service
 public interface StatementService {
 	
-	public List<Transactions> getMonthlyStatement(int accNo,int month,int year);
-	
-	public List<Transactions> getYearlyStatement(int accNo,int year);
-	
-//	@Query(nativeQuery = true, value="select t.trans_id from Transactions T where t.acct_no = :acct_no and t.trans_time between :startDate and :endDate")
-//	public List<Transactions> getCustomStatement(@Param("acct_no") int acct_no, @Param("startDate") LocalDate date, @Param("endDate") LocalDate date2);
-
-	public List<Transactions> getCustomStatement(LocalDate ld1,LocalDate ld2,int a);
+		public List<Transactions> getCustomStatementEmail(LocalDate ld1,LocalDate ld2,String e);
 }
